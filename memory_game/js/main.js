26d9845048a20621ac3b42a5defa4455e8c8823b
Mona Gaihab <monagaihab@Monas-MacBook-Air.local>
2019-11-07 14:38:28 +0300

    created game logic,function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,27 +4,20 @@ console.log("Up and running!");
 //var cardTwo = "queen" ;
 //var cardThree = "king" ;
 //var cardFour = "king" ;
-const cards = ["queen","queen","king","king"];
-const cardsInPlay = [];
-var cardOne = cards[2];
-var cardTwo = cards[3];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
 //console.log("User flipped " + cardOne) ;
 //console.log("User flipped " +cardThree) ;
-
 //alert('Hello, Friends ^_^');
-
-if(cardsInPlay.length === 2)
-{
-	if(cardsInPlay[0] === cardsInPlay[1])
-	{
-		alert("You found a match :)");
-	}
-	else
-	{
-		alert("Sorry, try again :(");
-	}
-}
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
+function checkForMatch(){
+	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+				console.log("You found a match :)");}
+		else{
+				console.log("Sorry, try again :(");}}}
+function flipCard(cardId){
+	console.log("User flipped "+ cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();}
+flipCard(0);
+flipCard(2);
